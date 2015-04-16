ba40bb1cefca4608fbc8335cd6df87154c87a375
Thiaxl <thiagosilva.vieira@hotmail.com>
2015-04-16 08:33:06 -0300

    Arquivos do projeto

---
+++ exemploGithub.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(){
+	int nota, contador = 1;
+	
+
+	while(contador < 11)
+	{
+		printf("Digite a nota do aluno %d:",contador);
+		scanf("%d[contador]", &nota);
+		printf("A nota do aluno %d eh: %d \n", contador, nota);
+		contador++;		
+ 	}	
+	system("pause");
+}
